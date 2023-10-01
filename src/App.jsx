127ab05bf938dae6b1c14f29ddd419f7cd1803a0
Changenot Damien <damien.changenot@gmail.com>
2023-10-01 17:52:07 +0200

    React Basics 06 - Pokédex - Utiliser un state

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,40 @@ const pokemonList = [
           "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-      name: "mew",
+    name: "charmander",
+    imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
   },
 ]
+
 function App() {
+  const [ pokemonIndex , setIndex ] = useState(0);
+
+  function handlePrevious() {
+    setIndex( pokemonIndex - 1)
+  }
+  function handleNext(){
+    setIndex( pokemonIndex + 1)
+  }
+
   return (
     <div>
-        <PokemonCard pokemon={pokemonList[0]}></PokemonCard>
+        <PokemonCard pokemon={pokemonList[pokemonIndex]}></PokemonCard>
+        { pokemonIndex ? <button  onClick={handlePrevious}>Précèdent</button> : ''}
+        { pokemonIndex < pokemonList.length - 1 ? <button  onClick={handleNext}>Suivant</button> : ''}
     </div>
   );
 }
