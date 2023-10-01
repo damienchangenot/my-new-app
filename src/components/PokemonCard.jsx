f142e409a6306c47fa32a0d63b2150dd3742efcf
Changenot Damien <damien.changenot@gmail.com>
2023-10-01 17:16:35 +0200

    fix

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,7 +2,7 @@ function PokemonCard(props) {
     const { pokemon } = props;
     return (
         <figure>
-            { pokemon.imgSrc === undefined ? <p>???</p> : <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur img" /> }
+            { pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc} alt="Bulbasaur img" /> }
             <figcaption>{pokemon.name}</figcaption>
         </figure>
     );
